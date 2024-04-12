39a7914edcd0d9868ef3b6b78c1d500803fd137e
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-04-12 15:49:15 +0200

    add .dark stylings to css

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,11 @@
 /* Dark Mode */
 
-/* Toggle Button */
+/* 1. in CSS verändertes Styling bei Hinzufügen von Klasse .dark  */
+/* 2. in JavaScript bei Click auf Checkbox Klasse dark in allen Elementen hinzufügen, die das betrifft */
+/* 3. if Klasse=dark in classList, dann Icons anderer Pfad (nämlich weiße bzw. hellgrüne) */
+/* 4. gleiches Vorgehen für die anderen Seiten (erst mal nur profile.html) */
+
+/* Toggle Button auf index.html */
 
 const answerButton = document.querySelector('[data-js="show-hide-button"]');
 
